e4a4447159754bf0c442c76bbd0269df61aa3abe
André Roaldseth <andre.roaldseth@nav.no>
2023-10-19 15:47:13 +0200

    Identifiser behov sendt ut av quiz

--- app/src/main/kotlin/no/nav/dagpenger/rapid/meldingskatalog/KjenteMeldinger.kt
+++ app/src/main/kotlin/no/nav/dagpenger/rapid/meldingskatalog/KjenteMeldinger.kt
@@ -3,6 +3,7 @@ package no.nav.dagpenger.rapid.meldingskatalog
 fun Meldingskatalog.kjenteMeldinger() {
     behov("Journalpost")
     behov("NySøknad")
+    behov("Barn", "faktum_svar") // Quiz sender behov med annen event type
     hendelse("faktum_svar")
     hendelse("søker_oppgave")
 }
--- meldingskatalog/src/main/kotlin/no/nav/dagpenger/rapid/meldingskatalog/BehovRiver.kt
+++ meldingskatalog/src/main/kotlin/no/nav/dagpenger/rapid/meldingskatalog/BehovRiver.kt
@@ -8,8 +8,8 @@ private class BehovRiver(
     rapidsConnection: RapidsConnection,
     meldingskatalog: Meldingskatalog,
     override val riverName: String,
+    override val eventName: String = "behov",
 ) : IdentifisertMeldingRiver(rapidsConnection, meldingskatalog) {
-    override val eventName: String = "behov"
     override fun validate(message: JsonMessage) {
         message.requireAllOrAny("@behov", listOf(riverName))
     }
@@ -17,6 +17,6 @@ private class BehovRiver(
     override fun createMessage(packet: JsonMessage) = Behov(riverName)
 }
 
-fun Meldingskatalog.behov(name: String) {
-    BehovRiver(delegatedRapid, this, name)
+fun Meldingskatalog.behov(name: String, eventName: String = "behov") {
+    BehovRiver(delegatedRapid, this, name, eventName)
 }
